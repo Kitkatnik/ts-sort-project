1098956eccdfe2b7fbd5f92ff75280a6131b4c1f
Katya <kittyserenakat@gmail.com>
2023-04-12 20:26:55 -0400

    🔨 refactor: extend Sorter class in CharactersCollection, NumbersCollection and LinkedList classes to avoid code duplication 🚀 chore: update index.ts to use Sorter class inheritance instead of creating a new instance of Sorter class

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection( [10, 3, -5, 0] );
-const sorter = new Sorter( numbersCollection );
-sorter.sort()
+numbersCollection.sort();
 console.log( numbersCollection.data ); 
 
 const charactersCollection = new CharactersCollection("Xaayb");
-const sorterChars = new Sorter( charactersCollection );
-sorterChars.sort();
+charactersCollection.sort();
 console.log( charactersCollection.data );
 
 const linkedList = new LinkedList();
@@ -19,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sortedList = new Sorter( linkedList );
-sortedList.sort();
+linkedList.sort();
 linkedList.print();
